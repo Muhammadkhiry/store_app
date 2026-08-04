e86155c9671fbebede83d97af7d24de6216b1dd0
Muhammad-Khairy <mohamedkhiry121t@gmail.com>
2026-08-04 16:44:12 +0300

    create the update product service

---
+++ lib/services/update_product.dart
@@ -0,0 +1,29 @@
+import 'package:dio/dio.dart';
+import 'package:store_app/models/product_model.dart';
+
+class UpdateProduct {
+  final Dio dio;
+
+  UpdateProduct({required this.dio});
+
+  Future<ProductModel> updateProduct({required ProductModel product}) async {
+    try {
+      final response = await dio.put(
+        "https://dummyjson.com/products/${product.id}",
+        data: {
+          "title": product.title,
+          "price": product.price,
+          "description": product.description,
+          "thumbnail": product.image,
+          "category": product.category,
+        },
+      );
+
+      return ProductModel.fromJson(response.data);
+    } on DioException catch (e) {
+      throw Exception(e.response?.data["message"] ?? e.message);
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+}
